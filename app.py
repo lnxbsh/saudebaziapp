c73b9780f8143606853478f3564a17844498e176
lnxbsh <c.honey95@gmail.com>
2023-04-18 16:59:42 +0530

    Third Commit to deploy

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
 import psycopg2
-import pandas as pd
 import re
 import json
+import os
 app = Flask(__name__)
-
 
 
 
@@ -14,21 +13,23 @@ def db(method='GET', data=''):
     conn = psycopg2.connect(
         host="dpg-cgtv1paut4mcfrnp2b70-a.singapore-postgres.render.com",
         database="saudebazi",
-        user="saudebaz",
-        password="emJNrMiFX83SRCwFcokOnwhzNJkaU7Fn")
+        user=os.getenv('username'),
+        password=os.getenv('password'))
     cur = conn.cursor()
     
     if data:
         if method == 'POST':
-            cur.excute('INSERT INTO COMMENTTABLE(entered_number, dialed_number, comment) VALUES(%s,%s,%s)',data['entered_number'],data['dialed_number'],data['comment'] )
-            cur.commit()
+            cur.execute('INSERT INTO COMMENT_DATA(entered_number, dialed_number, comment, timestamp) VALUES(%s,%s,%s,%s)',(data['entered_number'],data['dialed_number'],data['comment'],data['timestamp']), )
+            conn.commit()
+            cur.execute('SELECT * FROM COMMENT_DATA')
+            return cur.fetchall()
         elif method == 'PUT':
             print(data)
-            number = data['number']
-            info = data['info']
-            cur.execute("")
-            cur.execute('UPDATE CLEANEDTABLE SET VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) where "Mobile 1" LIKE %(number)s',(info[0],info[1],info[3],info[4],info[5],info[6],info[7],info[8],info[9],info[10],info[11],info[12],info[13],info[14],info[15],info[16],info[17],info[18]),{ 'number': '%{}%'.format(number)})
-            cur.commit()
+
+            for key, value in data.items():
+                cur.execute(f'UPDATE CLEANEDTABLE SET "{key}" =' +  ' %(val)s WHERE "Mobile 1" LIKE %(number)s',({"val":value,"number":'%{}%'.format(data["Mobile 1"])}))
+ #               cur.execute('UPDATE CLEANEDTABLE SET "%(col)s = %(val)s" WHERE "Mobile 1" LIKE %(number)s',({  "col": key,"val": value,'number': '%{}%'.format(data["Mobile 1"])}))
+            conn.commit()
             return "Data Updated"
         
         elif method == 'GET':    
@@ -44,7 +45,7 @@ def db(method='GET', data=''):
             print(cus)
             for d in cus.split('-'):
                 alist.append(fetchMatch(cur,d))
-            
+            return customer, alist
             # print(alist)
             # for a in alist:
             #     templist = []
@@ -55,38 +56,55 @@ def db(method='GET', data=''):
 
     cur.close()
     conn.close()
-    return customer, alist
+    
 
 
 
 
 
-@app.route('/matching', methods=["GET","POST","PUT"])
+@app.route('/matching', methods=["GET",'POST',"PUT"])
 def match():
     if request.method == 'GET':
         number = request.args.get('number')
         #print (number)
         fetch, leads = db(data=number)
         return render_template('match.html',data = [fetch[1:],leads])
-    
+        #return  json.dumps(db('POST', data))
+
     elif request.method == 'POST':
         print('POST is called')
-        entered_number = request.form['entered_number']
-        dialed_number = request.form['dialed_number']
-        comment = request.form['comment']
-        data = { 'entered_number':entered_number, 'dialed_number': dialed_number, 'comment':comment }
-        return  json.dumps(db('POST', data))
-    
+        formData = request.get_json("body")
+        entered_number = formData['entered_number']
+        dialed_number = formData['dialed_number']
+        comment = formData['comment']
+        timestamp = formData['timestamp']
+        data = { 'entered_number':entered_number, 'dialed_number': dialed_number, 'comment':comment, 'timestamp':timestamp }
+        comment_data= db('POST',data=data)
+        return json.dumps({"message":"Done", "comment_data":comment_data})
+
     elif request.method == 'PUT':
         data = request.get_json('body')
+        print(data)
         res = db(method='PUT',data=data)
         print("PUT is effective")
         return json.dumps(res)
+    
+# @app.route('/postdata',methods =["POST"])
+# def postData():
+    #  if request.method == 'POST':
+    #     print('POST is called')
+    #     formData = request.get_json("body")
+    #     entered_number = formData['entered_number']
+    #     dialed_number = formData['dialed_number']
+    #     comment = formData['comment']
+    #     data = { 'entered_number':entered_number, 'dialed_number': dialed_number, 'comment':comment }
+    #     db('POST',data=data)
+    #     return json.dumps("Done")
 
 
 def fetchMatch(cur,commoditiy):
     print(commoditiy)
-    cur.execute('SELECT * FROM cleanedtable WHERE "Current Commodity" LIKE %(name)s', { 'name': '%{}%'.format(commoditiy)})
+    cur.execute('SELECT * FROM cleanedtable WHERE "Current Commodity" LIKE %(name)s ORDER BY RANDOM() LIMIT 3', { 'name': '%{}%'.format(commoditiy)})
     #cur.execute("SELECT * FROM CLEANEDTABLE WHERE 'Current Commodity' = %s", commoditiy)
     return cur.fetchall()    
 
